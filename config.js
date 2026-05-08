867ec8b389bfcdd5aa61d624389347b7d8648cc2
over24hrs <153599939+over24hrs@users.noreply.github.com>
2026-05-08 19:42:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/YOUR_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxmCprVYm2pvaF6uNPhvHvJfZjtZAj7ZBQRYE4XKwJBAW-RrBa6SofWP0Emm5UD9TSaig/exec',
   APP_NAME: 'ระบบฌาปนกิจสงเคราะห์ ชุมชนแวนโค้ง'
 };
