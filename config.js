77e458f0a008986e8b459f2a2ba8fc63869e94ec
over24hrs <153599939+over24hrs@users.noreply.github.com>
2026-05-08 21:36:53 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbxypGlbJfSzrHgqMB4GRjin6E80glV9Q6AckgzuRhSN9pcz1BlQlJfiVZMy7yfixwi2sg/exec',
-    APP_NAME: 'ระบบฌาปนกิจสงเคราะห์ ชุมชนแวนโค้ง'
-  };
-  
